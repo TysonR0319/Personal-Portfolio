eab634aecdf7ad3d508f7936c3ff87cf97ad7607
TysonR0319 <tysonr0319@gmail.com>
2022-10-08 15:33:18 -0400

    finished skills and experience

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import './Skills.scss';
 import ReactToolTip from "react-tooltip"
 import { motion } from "framer-motion";
-import { AppWrap } from "../../wrapper";
+import { AppWrap, MotionWrap } from "../../wrapper";
 import { urlFor, client } from "../../client"
 
 const Skills = () => {
@@ -85,4 +85,7 @@ const Skills = () => {
   )
 }
 
-export default Skills
+export default AppWrap(
+  MotionWrap(Skills, "app__skills"),
+     "skills",
+     "app__whitebg");
